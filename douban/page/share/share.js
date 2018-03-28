81d316800f9852a27ad82e36e59194d8a2e6f034
Wangdandan <1558509669@qq.com>
2018-03-28 16:31:23 +0800

    first commit

---
+++ douban/page/share/share.js
@@ -0,0 +1,17 @@
+// pages/share/share.js
+Page({
+  data: {
+    imgUrl: 'https://www.newfq.com/assets/wx/movie-trailer/QRcode.png'
+  },
+  saveQRCode() {
+    const { imgUrl } = this.data
+    wx.downloadFile({
+      url: imgUrl,
+      success: (res) => {
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath
+        })
+      }
+    })
+  }
+})
---
+++ douban/page/subject/subject.js
@@ -0,0 +1,37 @@
+// pages/subject/subject.js
+Page({
+  data:{
+    types:[
+      {
+        name:'喜剧',
+        img:'/assets/image/xiju.png'
+      },
+      {
+        name: '动作',
+        img: '/assets/image/dongzuo.png'
+      },
+      {
+        name: '科幻',
+        img: '/assets/image/kehuan.png'
+      },
+      {
+        name: '爱情',
+        img: '/assets/image/aiqing.png'
+      },
+      {
+        name: '惊悚',
+        img: '/assets/image/jingsong.png'
+      },
+      {
+        name: '动画',
+        img: '/assets/image/donghua.png'
+      }
+    ]
+  },
+  typeHandler(e){
+    const {type} = e.currentTarget.dataset
+    wx.navigateTo({
+      url:'../subject-list/subject-list?type='+type,
+    })
+  }
+})
